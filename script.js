4f6db2c9fa9f21c20c5f4637b20272fd19f197be
izzy <isabella.wang91@gmail.com>
2021-11-01 23:44:51 -0400

    created a search bar that appears on clicking the magnifying glass (header)

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ const emailSubmission = document.querySelector('.email-submission');
 
 const emailInput = document.querySelector('.subscribe-email');
 
+// when the li '.search' is clicked
+const search = document.querySelector('.search');
+console.log(search)
+
+search.addEventListener('click', function(){
+  console.log('clicky clicky');
+  // creates a form
+  const searchBar = document.createElement('form');
+  // adds the .search-bar class
+  searchBar.classList.add('search-bar');
+  // selects the .search-bar class
+  const searchBarClass = document.querySelector('.search-bar');
+  // if the .search-bar class does not exist, do the following (will only append once)
+  if (searchBarClass === null){
+  searchBar.innerHTML = `<input type="text" name="searchBar" id="searchBar" class="searchBar" placeholder="Search" required>
+  <label for="searchBar" class="sr-only">Enter your search query</label>
+  <button type="submit"><i class="fas fa-long-arrow-alt-right"></i></button>`;
+  search.appendChild(searchBar);
+  } else{
+    // if the search-bar class exists, remove it
+    searchBarClass.remove();
+  }
+});
 
 
 
