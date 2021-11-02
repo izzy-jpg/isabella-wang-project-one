c079bfb18682e0eac14d09ff792ebc14af4e31db
izzy <isabella.wang91@gmail.com>
2021-11-01 23:08:24 -0400

    added an alert on email subscription (footer)

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// when the form '.email-submission' is submitted
+const emailSubmission = document.querySelector('.email-submission');
+
+const emailInput = document.querySelector('.subscribe-email');
+
+
+
+
+
+function init(){
+  emailSubmission.addEventListener('submit', function(e){
+    e.preventDefault();
+  
+    // clear the input
+    emailInput.value = '';
+  
+    // create an alert that says 'thank you for subscribing'
+    alert("Thank you for subscribing!");
+  })
+};
+
 const docReady = function() {
   if (document.readyState === ('complete')) {
     init();
