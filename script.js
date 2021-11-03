cd9c9d4211a6ec3f9470da77c03c4166c0e1785a
izzy <isabella.wang91@gmail.com>
2021-11-03 18:01:16 -0400

    organized code

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-// selects <form> email-submission and <input> subscribe-email
-const emailSubmission = document.querySelector('.email-submission');
-const emailInput = document.querySelector('.subscribe-email');
 
+const submitEmail = function () {
 
-// selects <i> fa-search and <li> search
-const searchIcon = document.querySelector('.fa-search');
-const search = document.querySelector('.search');
+  // selects <form> email-submission and <input> subscribe-email
+  const emailSubmission = document.querySelector('.email-submission');
+  const emailInput = document.querySelector('.subscribe-email');
 
-
-
-const submitEmail = function() {
   // when the <form> '.email-submission' is submitted
-  emailSubmission.addEventListener('submit', function(e){
+  emailSubmission.addEventListener('submit', function (e) {
     e.preventDefault();
-  
+
     // clear the input
     emailInput.value = '';
-  
+
     // create an alert that says 'Thank you for subscribing!'
     alert("Thank you for subscribing!");
   });
@@ -24,30 +19,46 @@ const submitEmail = function() {
 
 
 
-const addSearchBar = function() {
+
+
+
+const addSearchBar = function () {
+
+  // selects <i> fa-search and <li> search
+  const searchIcon = document.querySelector('.fa-search');
+  const search = document.querySelector('.search');
+
   // when the <i> '.fa-search' is clicked
-  searchIcon.addEventListener('click', function(){
+  searchIcon.addEventListener('click', function () {
+  
     // creates a form
     const searchBar = document.createElement('form');
+  
     // adds the .search-bar class (which has the css for the bar on it)
     searchBar.classList.add('search-bar');
+  
     // selects the .search-bar class we just created
     const searchBarClass = document.querySelector('.search-bar');
+  
     // if the .search-bar class does not exist, do the following (will only append once)
-    if (searchBarClass === null){
-    searchBar.innerHTML = `<input type="text" name="searchBar" id="searchBar" class="searchBar" placeholder="Search" required>
+    if (searchBarClass === null) {
+      searchBar.innerHTML = `<input type="text" name="searchBar" id="searchBar" class="searchBar" placeholder="Search" required>
     <label for="searchBar" class="sr-only">Enter your search query</label>
     <button type="submit"><i class="fas fa-long-arrow-alt-right"></i></button>`;
-    search.appendChild(searchBar);
-    } else{
+      search.appendChild(searchBar);
+    } else {
       // if the search-bar class exists, remove it
       searchBarClass.remove();
     }
   });
 };
 
+
+
+
+
 // initializes
-function init(){
+function init() {
 
   // calls submitEmail and addSearchBar functions
   submitEmail();
@@ -56,12 +67,12 @@ function init(){
 
 
 // when the document is ready, run init()
-const docReady = function() {
+const docReady = function () {
   if (document.readyState === ('complete')) {
     init();
   }
   else {
-    document.addEventListener ('DOMContentLoaded', init)
+    document.addEventListener('DOMContentLoaded', init)
   }
 }
 docReady();
